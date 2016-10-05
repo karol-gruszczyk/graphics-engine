f311e744e1d882a2a8c43a4dc5eb1c6bf0af6d60
karol-gruszczyk <karol.gruszczyk@gmail.com>
2016-10-05 21:32:49 +0200

    fixed uniform location assert

--- include/bauasian/shaders/shader_program.cpp
+++ include/bauasian/shaders/shader_program.cpp
@@ -51,7 +51,7 @@ const GLint& ShaderProgram::getUniformLocation(const std::string& uniform_name)
 {
 	use();
 	const auto& location = glGetUniformLocation(m_shader_program_id, uniform_name.c_str());
-	assert(location != -1);
+	assert(location != GL_INVALID_INDEX);
 	return location;
 }
 
