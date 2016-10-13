b6e3b232629e76f610714ead997b34ed1ebcfdb5
karol-gruszczyk <karol.gruszczyk@gmail.com>
2016-10-13 14:25:21 +0200

    added DSA to cube textures

--- include/bauasian/materials/cube_texture.cpp
+++ include/bauasian/materials/cube_texture.cpp
@@ -11,16 +11,15 @@ CubeTexture::CubeTexture(const std::vector<unsigned>& widths, const std::vector<
 {
 	assert((widths.size() == heights.size()) == (pixel_ptrs.size() == 6));
 
-	glBindTexture(GL_TEXTURE_CUBE_MAP, m_texture_id);
 	for (unsigned i = 0; i < 6; i++)
 	{
-		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internal_format, widths[i], heights[i],
-		             0, format, GL_UNSIGNED_BYTE, pixel_ptrs[i]);
+		glTextureImage2DEXT(m_texture_id, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internal_format,
+		                    widths[i], heights[i], 0, format, GL_UNSIGNED_BYTE, pixel_ptrs[i]);
 	}
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+	glTextureParameteri(m_texture_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTextureParameteri(m_texture_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
