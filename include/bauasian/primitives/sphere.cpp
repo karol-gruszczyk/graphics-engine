13d16127d28773705848440eb6e10f59f1fdd029
karol-gruszczyk <karol.gruszczyk@gmail.com>
2016-10-20 17:15:16 +0200

    added uvs to sphere

--- include/bauasian/primitives/sphere.cpp
+++ include/bauasian/primitives/sphere.cpp
@@ -14,14 +14,15 @@ Sphere::Sphere(const float& radius, const unsigned& num_sides)
 	auto num_vertices = getNumVertices();
 	Vertex3D* vertices = new Vertex3D[num_vertices];
 
-	unsigned index = 0;
-	auto process_vertex = [](Vertex3D& vertex, const glm::vec3& pos) -> void
+	auto process_vertex = [](Vertex3D& vertex, const glm::vec3& pos, const float& r) -> void
 	{
-		vertex.position = pos;
+		vertex.position = pos * r;
 		vertex.normal = glm::normalize(pos);
-		vertex.uv = glm::vec2(vertex.position.x, vertex.position.z);
+		vertex.uv = glm::vec2((pos.x * pos.z) / 2.f + 0.5f, pos.y / 2.f + 0.5f);
 	};
-	process_vertex(vertices[index], glm::vec3(0.f, m_radius, 0.f));
+
+	unsigned index = 0;
+	process_vertex(vertices[index], glm::vec3(0.f, 1.f, 0.f), m_radius);
 	for (unsigned i = 1; i < num_sides; i++)
 	{
 		double h_angle = glm::pi<float>() * i / num_sides;
@@ -30,10 +31,10 @@ Sphere::Sphere(const float& radius, const unsigned& num_sides)
 			double angle = glm::two_pi<float>() * j / num_sides;
 			process_vertex(vertices[++index], glm::vec3(glm::sin(angle) * glm::sin(h_angle),
 														glm::cos(h_angle),
-														glm::cos(angle) * glm::sin(h_angle)) * m_radius);
+														glm::cos(angle) * glm::sin(h_angle)), m_radius);
 		}
 	}
-	process_vertex(vertices[++index], glm::vec3(0.f, -m_radius, 0.f));
+	process_vertex(vertices[++index], glm::vec3(0.f, -1.f, 0.f), m_radius);
 
 	auto num_indices = getNumElements();
 	GLuint* indices = new GLuint[num_indices];
